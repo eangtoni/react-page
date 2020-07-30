c59d9887a1e8e4bb082570c107faba7099ce450e
eangtoni <erik.angtoni88@gmail.com>
2020-07-30 17:48:38 +0800

    Updates

---
+++ precache-manifest.578d69dcad900caf00ec92c69d948435.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ae9d66bd4c1b34a77d37579018ff19",
+    "url": "/react-page/index.html"
+  },
+  {
+    "revision": "b80e4993ec3b0fe93d6a",
+    "url": "/react-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2aa4ddb3f02455901f26",
+    "url": "/react-page/static/js/2.0928ce5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-page/static/js/2.0928ce5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b80e4993ec3b0fe93d6a",
+    "url": "/react-page/static/js/main.f3f1b892.chunk.js"
+  },
+  {
+    "revision": "950d8cdf0317e0a9785e",
+    "url": "/react-page/static/js/runtime-main.3b5893a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
